151e0fa5178282e497ea675c116117278ce8a273
oigabrielteodoro <oi@gabrielteodoro.com>
2021-05-03 20:51:06 -0300

    feat: added next auth

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -3,6 +3,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       STRIPE_API_KEY: string;
+      GITHUB_CLIENT_ID: string;
+      GITHUB_CLIENT_SECRET: string;
     }
   }
 }
--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,29 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  function handleSignIn() {
+    signIn('github');
+  }
+
+  function handleSignOut() {
+    signOut();
+  }
+
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={handleSignOut}
     >  
       <FaGithub color="#04d361" />
-      Gabriel Teodoro
+      {session.user?.name}
 
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
@@ -20,6 +31,7 @@ export function SignInButton() {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={handleSignIn}
     >
       <FaGithub color="#eba417" />
       Sign In with Github
