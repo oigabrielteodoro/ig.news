0e0642a21cb6f36214ef564c9361c645d04fac92
oigabrielteodoro <oi@gabrielteodoro.com>
2021-05-15 14:22:11 -0300

    feat: configured faunadb in application

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query } from 'faunadb';
+
 import NextAuth from 'next-auth'
+
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +14,38 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      const { email } = user;
+   
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email as string)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'), 
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(email as string)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 })
