a448b0884f5ebd0c046da73c9dc2b23d3be7e0a9
oigabrielteodoro <oi@gabrielteodoro.com>
2021-05-20 17:04:15 -0300

    feat: added types in getStaticPaths within post preview page

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/client";
 
 import Head from 'next/head';
@@ -63,7 +63,7 @@ type Params = {
   slug: string;
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
